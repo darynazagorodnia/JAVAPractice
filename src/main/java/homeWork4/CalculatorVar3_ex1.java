f1fb6a6fdf3feb5fe05d20bbba046436e990f9eb
darynazagorodnia <darynazagorodnia@gmail.com>
2026-03-11 22:52:30 +0200

    Зробила калькулятор з виловом помилок та з класом enum.

--- src/main/java/homeWork4/CalculatorVar3_ex1.java
+++ src/main/java/homeWork4/CalculatorVar3_ex1.java
@@ -1,5 +1,6 @@
 package homeWork4;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalculatorVar3_ex1 {
@@ -29,35 +30,57 @@ public class CalculatorVar3_ex1 {
         Scanner scanner = new Scanner(System.in);
         boolean continueCalc = true;
         CalculatorVar3_ex1 myCalc = new CalculatorVar3_ex1();
-        do {
-            System.out.println("Enter first number: ");
-            double a = scanner.nextDouble();
-            System.out.println("Enter operator: ");
-            char operator = scanner.next().charAt(0);
-            System.out.println("Enter second number: ");
-            double b = scanner.nextDouble();
+        try {
+            do {
+                double a, b;
+                Operations selectOp;
+                try {
+                    System.out.println("Enter first number: ");
+                    a = scanner.nextDouble();
+                } catch (InputMismatchException e) {
+                    System.out.println("It isn't a number!");
+                    scanner.next();
+                    continue;
+                }
+                System.out.println("Enter operator: ");
+                char inputSymbol = scanner.next().charAt(0);
+                selectOp = Operations.fromChar(inputSymbol);
+                try {
+                    System.out.println("Enter second number: ");
+                    b = scanner.nextDouble();
+                } catch (InputMismatchException e) {
+                    System.out.println("It isn't number!");
+                    scanner.next();
+                    continue;
+                }catch (IllegalArgumentException e){
+                    System.out.println("Unknown operator!");
+                    scanner.next();
+                    continue;
+                }
+                switch (selectOp) {
+                    case ADD -> System.out.println("Result: " + myCalc.add(a, b));
+                    case SUBTRACT -> System.out.println("Result: " + myCalc.subtract(a, b));
+                    case MULTIPLY -> System.out.println("Result: " + myCalc.multiply(a, b));
+                    case DIVIDE -> System.out.println("Result: " + myCalc.divide(a, b));
+                    default -> System.out.println("Ups!");
+                }
+                System.out.println("Continue? (Yes/No)");
+                char choice = scanner.next().charAt(0);
+                if (choice == 'n') {
+                    continueCalc = false;
+                    System.out.println("Good buy!");
 
-            switch (operator) {
-                case '+' -> System.out.println("Result: " + myCalc.add(a, b));
-                case '-' -> System.out.println("Result: " + myCalc.subtract(a, b));
-                case '*' -> System.out.println("Result: " + myCalc.multiply(a, b));
-                case '/' -> System.out.println("Result: " + myCalc.divide(a, b));
-                default -> System.out.println("Ups!");
-
-            }
-            System.out.println("Continue? (Yes/No)");
-            char choice = scanner.next().charAt(0);
-            if (choice == 'n') {
-                continueCalc = false;
-                System.out.println("Good buy!");
-
-            } else if (choice == 'y') {
-                continueCalc = true;
-                System.out.println("oK!");
-            }
-        } while (continueCalc);
+                } else if (choice == 'y') {
+                    continueCalc = true;
+                    System.out.println("oK!");
+                }
+            } while (continueCalc);
+        } finally {
+            System.out.println("Сlosed safely!");
+        }
         scanner.close();
     }
+
 
 }
 
