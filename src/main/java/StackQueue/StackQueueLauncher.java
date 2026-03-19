0a1e993e8b8792aba5f5a58db03e7d34ca30dc02
darynazagorodnia <darynazagorodnia@gmail.com>
2026-03-19 22:50:57 +0200

    Колекції, Розділ 6 54-62.

--- src/main/java/StackQueue/StackQueueLauncher.java
+++ src/main/java/StackQueue/StackQueueLauncher.java
@@ -1,25 +1,52 @@
 package StackQueue;
 
-import java.util.Stack;
+import colectionClass.Card;
+import colectionClass.CardComparator;
+
+import java.util.*;
 
 public class StackQueueLauncher {
     public static void main(String[] args) {
-        passengerProcession();
+
+        //passengerProcession();
+
+        Queue<Card> cardDeck = new PriorityQueue<>(52, new CardComparator());
+        for (Card.Face face : Card.Face.values()) {
+            for (Card.Suit suit : Card.Suit.values()) {
+                cardDeck.offer(new Card(suit, face));
+            }
+        }
+
+        Deque<Card> cards = new ArrayDeque<>();
+        for (int i = 0; i < 10; i++) {
+            cards.offerLast(cardDeck.poll());
+        }
+
+        //  for (int i = 0; i < 10; i++) {
+        //    System.out.println(cards.removeFirst());
+        //}
+
+        Card card = new Card(Card.Suit.SPADES, Card.Face.Ten);
+        cards.removeFirstOccurrence(card);
+        cards.removeLastOccurrence(card);
+
+
     }
+
 
     private static void passengerProcession() {
         Stack<Passenger> bus = new Stack<>();
         Passenger passenger = new Passenger("Ivanova", "Katerina");
-        bus.push(new Passenger("Vasko","Alex"));
-        bus.push(new Passenger("Mikhailov","Viktor"));
-        bus.push(new Passenger("Petrov","Dmitrii"));
+        bus.push(new Passenger("Vasko", "Alex"));
+        bus.push(new Passenger("Mikhailov", "Viktor"));
+        bus.push(new Passenger("Petrov", "Dmitrii"));
         bus.push(passenger);
         bus.push(new Passenger("Ivanov", "Ivan"));
 
         System.out.println("Passenger found at position " + bus.search(passenger));
 
         System.out.println("Last enter passenger " + bus.peek());
-        while (!bus.empty()){
+        while (!bus.empty()) {
             System.out.println("Passenger: " + bus.pop());
         }
 
@@ -37,14 +64,22 @@ public class StackQueueLauncher {
         }
 
 
-        public int getNumber(){return number;}
-        public String getName(){return name;}
-        public String getSurname(){
+        public int getNumber() {
+            return number;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getSurname() {
             return surname;
         }
 
         @Override
-        public String toString() { return "Passenger is " + name + " " + surname;}
+        public String toString() {
+            return "Passenger is " + name + " " + surname;
+        }
     }
 }
 
---
+++ src/main/java/map/MapLauncher.java
@@ -0,0 +1,34 @@
+package map;
+
+import Set.Car;
+
+import java.util.*;
+
+public class MapLauncher {
+    public static void main(String[] args) {
+        Map<String, Integer> wordMap = new HashMap<>();
+        System.out.println("Please enter some text");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        String[] tokens = string.split("");
+        for (String token : tokens) {
+            String word = token.toLowerCase();
+            Integer count = wordMap.get(word);
+            if (count == null) {
+                wordMap.put(word, 1);
+            } else {
+                wordMap.put(word, count + 1);
+            }
+        }
+        printMap(wordMap);
+
+    }
+    private static void printMap(Map<String, Integer>wordMap){
+        Map<String, Integer>wordTreeMap = new TreeMap<>(wordMap);
+        Set<String> keys = wordMap.keySet();
+        for (String key:keys){
+            System.out.printf("%-10s%-10s\n", key, wordMap.get(key));
+        }
+
+    }
+}
